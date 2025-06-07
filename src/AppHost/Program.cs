8c0c237f855613359b218ab86c0385eb265b567d
filipe <felipe_157@windowslive.com>
2025-06-06 22:53:16 -0400

    feat: Update PostgreSQL configuration in Program.cs for consistent authentication and volume management

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -2,19 +2,17 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var databaseName = "ServerGameDb";
-
+var postgresPassword = builder.AddParameter("postgres-password", "aspire123!", secret: true);
 var postgres = builder
-    .AddPostgres("postgres")
+    .AddPostgres("postgres", password: postgresPassword) // Fixed password for consistent authentication
     // Set the name of the default database to auto-create on container startup.
-    .WithEnvironment("POSTGRES_DB", databaseName)
-    .WithDataVolume("postgres-data"); // Volume for PostgreSQL data
+    .WithVolume("postgres-data", "/var/lib/postgresql/data") // <-- Volume aplicado CORRETAMENTE ao SERVIDOR
+    .WithPgAdmin();
 
-var database = postgres.AddDatabase(databaseName);
-
+var serverdb = postgres.AddDatabase("serverdb", "serverdb");
 builder
     .AddProject<Projects.API>("ApiService")
-    .WithReference(database)
-    .WaitFor(database);
+    .WithReference(serverdb)
+    .WaitFor(serverdb);
 
 builder.Build().Run();

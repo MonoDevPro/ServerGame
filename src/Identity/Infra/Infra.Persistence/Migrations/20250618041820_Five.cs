ccf0764524adb8102bb29e89464ec943a7307ca4
filipe <felipe_157@windowslive.com>
2025-06-18 00:37:32 -0400

    feat: Add initial database migration for identity and game server schemas

---
+++ src/Identity/Infra/Infra.Persistence/Migrations/20250618041820_Five.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServerGame.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Five : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                schema: "gameserver",
+                table: "Accounts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                schema: "gameserver",
+                table: "Accounts",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}

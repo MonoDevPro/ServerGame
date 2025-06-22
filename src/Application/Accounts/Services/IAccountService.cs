b1986c43d6b02511653233bfe13ff2592e2b9e06
filipe <felipe_157@windowslive.com>
2025-06-22 13:06:43 -0400

    feat: Refactor AccountService to enhance account retrieval and creation methods

--- src/Application/Accounts/Services/IAccountService.cs
+++ src/Application/Accounts/Services/IAccountService.cs
@@ -1,3 +1,4 @@
+using GameServer.Application.Accounts.Queries.Models;
 using GameServer.Domain.Entities;
 
 namespace GameServer.Application.Accounts.Services;
@@ -5,8 +6,9 @@ namespace GameServer.Application.Accounts.Services;
 public interface IAccountService
 {
     Task<bool> ExistsAsync(CancellationToken cancellationToken = default);
-    Task<Account> GetAsync(CancellationToken cancellationToken = default);
-    Task<Account> CreateAsync(Account account, CancellationToken cancellationToken = default);
-    Task<Account> UpdateAsync(Account account, CancellationToken cancellationToken = default);
+    Task<Account> GetReadOnlyAsync(CancellationToken cancellationToken = default);
+    Task<Account> GetForUpdateAsync(CancellationToken cancellationToken = default);
+    Task<AccountDto> GetDtoAsync(CancellationToken cancellationToken = default);
+    Task<Account> CreateAsync(CancellationToken cancellationToken = default);
     Task PurgeAsync(CancellationToken cancellationToken = default);
 }
--- src/Infrastructure/Services/AccountService.cs
+++ src/Infrastructure/Services/AccountService.cs
@@ -1,13 +1,17 @@
+using AutoMapper;
+using GameServer.Application.Accounts.Queries.Models;
 using GameServer.Application.Accounts.Services;
 using GameServer.Application.Common.Interfaces;
 using GameServer.Application.Common.Interfaces.Persistence;
 using GameServer.Application.Common.Interfaces.Persistence.Repository;
 using GameServer.Domain.Entities;
+using GameServer.Domain.Exceptions;
 
 namespace GameServer.Infrastructure.Services;
 
 public class AccountService(
     IUser user,
+    IMapper mapper,
     IRepositoryCompose<Account> accountRepository)
     : IAccountService
 {
@@ -17,72 +21,55 @@ public class AccountService(
 
     public async Task<bool> ExistsAsync(CancellationToken cancellationToken = default)
     {
-        // 2) Execute a consulta e retorne o resultado:
         return await ReaderAccountRepository
             .ExistsAsync(a => a.CreatedBy == user.Id,
                 cancellationToken: cancellationToken);
     }
-
-    public async Task<Account> GetAsync(CancellationToken cancellationToken = default)
-    {
-        // 2) Monte a expressão para o EF (que compara duas strings):
-        return await ReaderAccountRepository
+    
+    public async Task<Account> GetReadOnlyAsync(CancellationToken cancellationToken = default) =>
+        await ReaderAccountRepository
+            .QuerySingleAsync<Account>(
+                predicate: a => a.CreatedBy == user.Id,
+                trackingType: TrackingType.NoTracking,
+                cancellationToken: cancellationToken) 
+        ?? throw new KeyNotFoundException($"Account with userId '{user.Id}' not found.");
+    public async Task<Account> GetForUpdateAsync(CancellationToken cancellationToken = default) =>
+        await ReaderAccountRepository
             .QuerySingleAsync<Account>(
                 predicate: a => a.CreatedBy == user.Id,
                 trackingType: TrackingType.Tracking,
-                cancellationToken: cancellationToken) ?? throw new KeyNotFoundException($"Account with userId '{user.Id}' not found.");
-    }
+                cancellationToken: cancellationToken) 
+        ?? throw new KeyNotFoundException($"Account with userId '{user.Id}' not found.");
 
-    public async Task<Account> CreateAsync(Account account, CancellationToken cancellationToken = default)
+    public async Task<AccountDto> GetDtoAsync(CancellationToken cancellationToken = default)
     {
-        account = await WriterAccountRepository.AddAsync(account, cancellationToken);
-        
-        return await WriterAccountRepository.SaveChangesAsync(cancellationToken)
-            ? account
-            : throw new InvalidOperationException("Failed to create account in the database.");
+        return await ReaderAccountRepository
+                   .QuerySingleAsync<AccountDto>(
+                       predicate: a => a.CreatedBy == user.Id,
+                       selector: a => mapper.Map<AccountDto>(a),
+                       trackingType: TrackingType.NoTracking,
+                       cancellationToken: cancellationToken) 
+               ?? throw new NotFoundException(
+                   user.Id ?? String.Empty, 
+                   nameof(user.Id) , 
+                   new DomainException($"Account with userId '{user.Id}' not found."));
     }
-
-    public async Task<Account> UpdateAsync(Account account, CancellationToken cancellationToken = default)
-    {
-        await WriterAccountRepository.UpdateAsync(account, cancellationToken);
-        return await WriterAccountRepository.SaveChangesAsync(cancellationToken)
-            ? account
-            : throw new InvalidOperationException("Failed to update account in the database.");
-    }
+    
+    public async Task<Account> CreateAsync(CancellationToken cancellationToken = default) 
+        => await WriterAccountRepository.AddAsync(Account.Create(), cancellationToken);
 
     public async Task PurgeAsync(CancellationToken cancellationToken = default)
     {
-        try
-        {
-            var accounts = await ReaderAccountRepository
-                .QueryListAsync<Account>(
-                    trackingType: TrackingType.Tracking,
-                    cancellationToken: cancellationToken);
+        var accounts = await ReaderAccountRepository
+            .QueryListAsync<Account>(
+                trackingType: TrackingType.Tracking,
+                cancellationToken: cancellationToken);
             
-            if (accounts.Count == 0)
-                throw new InvalidOperationException("No accounts found to purge.");
-            
-            foreach (var account in accounts)
-            {
-                // 1) Remove all roles from the account:
-                account.Deactivate();
-                
-                // 2) Remove the account from the repository:
-                await WriterAccountRepository.UpdateAsync(account, cancellationToken);
-            }
-            
-            // 3) Save changes to the database:
-            var saveResult = await WriterAccountRepository.SaveChangesAsync(cancellationToken);
+        accounts.ForEach(a => a.Deactivate());
+    }
 
-            if (!saveResult)
-            {
-                throw new InvalidOperationException("Failed to delete all accounts from the database.");
-            }
-        }
-        catch (Exception ex)
-        {
-            // Log the exception or handle it as needed
-            throw new InvalidOperationException("An error occurred while purging accounts.", ex);
-        }
+    private async Task<bool> SaveAsync(CancellationToken cancellationToken = default)
+    {
+        return await WriterAccountRepository.SaveChangesAsync(cancellationToken);
     }
 }
